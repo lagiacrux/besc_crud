b2196c347346766f0c3324a4493ffcf629548fac
lagiacrux <benjamin.schneller@gmx.at>
2015-09-09 16:03:30 +0200

    messages are now only available in the edit_view

--- items/besc_crud/js/besc_crud.js
+++ items/besc_crud/js/besc_crud.js
@@ -1,53 +1,9 @@
-var bc_message_phasing = 200;
-var bc_message_id = 0;
+
 
 $(document).ready(function()
 {	
-	positionMessageContainer();
+	
 });
 
 
-function positionMessageContainer()
-{
-	var parent = $('.bc_message_container').parent();
-	var width = parent.width() + parseInt(parent.css('margin-left')) + parseInt(parent.css('margin-right')) + parseInt(parent.css('padding-left')) + parseInt(parent.css('padding-right'));
-	$('.bc_message_container').css({'left': parent.position().left, 'top': parent.position().top, 'width': width});
-}
 
-
-function showMessage(type, message)
-{
-	switch(type)
-	{
-		case 'error':
-			var bg_class = "bc_message_error";
-			break;
-		case 'success':
-			var bg_class = "bc_message_success";
-			break;
-	}
-	
-	$('.bc_message_container').prepend('<div bc_message_id="' + bc_message_id + '" class="bc_message ' + bg_class + '">' + message + '</div>');
-	$('.bc_message[bc_message_id="' + bc_message_id + '"]').animate({'line-height': 40}, bc_message_phasing, function()
-	{
-		$(this).click(function()
-		{
-			$(this).animate({'line-height': 0}, bc_message_phasing, function()
-			{
-				$(this).remove();
-			});
-		});
-		setTimeoutMessageDisappear($(this))
-	});
-	bc_message_id++;
-	
-	//alert(message);
-}
-
-function setTimeoutMessageDisappear(message)
-{
-	setTimeout(function()
-	{
-		message.click();
-	}, 3000);
-}
--- items/besc_crud/js/besc_crud_edit.js
+++ items/besc_crud/js/besc_crud_edit.js
@@ -1,4 +1,7 @@
 var bc_and_go_back = false;
+var bc_message_phasing = 200;
+var bc_message_id = 0;
+var bc_message_lingering = 4000;
 
 $(document).ready(function()
 {	
@@ -12,6 +15,7 @@ $(document).ready(function()
 	bc_addMultilineListeners();
 	bc_addDatepickerListeners();
 	bc_addComboboxListeners();
+	bc_positionMessageContainer();
 });
 
 
@@ -525,4 +529,48 @@ function getCSS(prop, fromClass)
     } finally {
         $inspector.remove(); // and remove from DOM
     }
+}
+
+
+function bc_positionMessageContainer()
+{
+	var parent = $('.bc_message_container').parent();
+	var width = parent.width() + parseInt(parent.css('margin-left')) + parseInt(parent.css('margin-right')) + parseInt(parent.css('padding-left')) + parseInt(parent.css('padding-right'));
+	$('.bc_message_container').css({'left': parent.position().left, 'top': parent.position().top, 'width': width});
+}
+
+
+function showMessage(type, message)
+{
+	switch(type)
+	{
+		case 'error':
+			var bg_class = "bc_message_error";
+			break;
+		case 'success':
+			var bg_class = "bc_message_success";
+			break;
+	}
+	
+	$('.bc_message_container').prepend('<div bc_message_id="' + bc_message_id + '" class="bc_message ' + bg_class + '">' + message + '</div>');
+	$('.bc_message[bc_message_id="' + bc_message_id + '"]').animate({'line-height': 40}, bc_message_phasing, function()
+	{
+		$(this).click(function()
+		{
+			$(this).animate({'line-height': 0}, bc_message_phasing, function()
+			{
+				$(this).remove();
+			});
+		});
+		setTimeoutMessageDisappear($(this))
+	});
+	bc_message_id++;
+}
+
+function setTimeoutMessageDisappear(message)
+{
+	setTimeout(function()
+	{
+		message.click();
+	}, bc_message_lingering);
 }
